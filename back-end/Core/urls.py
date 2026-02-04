ec86b041aa1082a2fe5dae220a72af4fb31331f6
erfan mahmoudi <erfanmahmudi1383@gmail.com>
2026-02-04 15:50:46 +0330

    add: backend starting auth files

---
+++ back-end/Core/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AuthViewSet, UserViewSet, HomeAPIView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from rest_framework.permissions import AllowAny
+
+# Custom router to allow anonymous access to root
+class CustomRouter(DefaultRouter):
+    def get_api_root_view(self, api_urls=None, **kwargs):
+        view = super().get_api_root_view(api_urls=api_urls, **kwargs)
+        view.cls.permission_classes = [AllowAny]
+        return view
+
+router = CustomRouter()
+router.register(r'auth', AuthViewSet, basename='auth')
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('api/v1/', include([
+        path('', router.get_api_root_view(), name='api-root'),  # Public root page
+        path('', include(router.urls)),                         # /auth/, /users/
+        
+        # Standard JWT endpoints
+        path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    ])),
+]
--- back-end/hotel_booking/hotel_booking/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('reservations.urls')),
-    path('admin/', admin.site.urls),
-    path('api/', include('reservations.urls')),
-]
