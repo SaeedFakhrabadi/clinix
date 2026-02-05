2c7a2dea45b45b409176fa4cc2cebbf3aa18ab04
erfan mahmoudi <erfanmahmudi1383@gmail.com>
2026-02-05 14:25:38 +0330

    fix: CORS problem fixed

--- back-end/clinix/settings.py
+++ back-end/clinix/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Core',
+    'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
@@ -64,6 +65,8 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,3 +160,7 @@ EMAIL_PORT=465 # SSL
 EMAIL_USE_SSL=True
 EMAIL_HOST_USER="erfanmahmudi1383@gmail.com"
 EMAIL_HOST_PASSWORD="tszw lset sxkm band"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
