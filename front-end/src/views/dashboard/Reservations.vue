19dcf4ab6c9efb91576c12c24ec6becc7c0781d9
SaeedFakhrabadi <saeedfakhrabadi8@gmail.com>
2026-02-20 12:11:23 -0800

    add: reservations page for doctor role

--- front-end/src/views/dashboard/Reservations.vue
+++ front-end/src/views/dashboard/Reservations.vue
@@ -19,27 +19,60 @@
 	const currentUserStore = useCurrentUserStore();
 	const { currentUser } = storeToRefs(currentUserStore);
 
+	const isPatient = computed(
+		() => currentUser.value?.role?.value === 'PATIENT',
+	);
+	const isDoctor = computed(() => currentUser.value?.role?.value === 'DOCTOR');
+
 	const reservationId = ref(0);
+	const userId = ref(0);
 
 	const modalText = ref('');
 	const isModalOpen = ref(false);
 
-	const tableHeaders = [
-		{ label: 'نام پزشک', value: 'doctor_name' },
-		{ label: 'تاریخ نوبت', value: 'date' },
-		{ label: 'ساعت', value: 'hour' },
-		{ label: 'وضعیت', value: 'is_active' },
-	];
+	const tableHeaders = computed(() => {
+		if (isPatient.value) {
+			return [
+				{ label: 'نام پزشک', value: 'doctor_name' },
+				{ label: 'تاریخ نوبت', value: 'date' },
+				{ label: 'ساعت', value: 'hour' },
+				{ label: 'وضعیت', value: 'is_active' },
+			];
+		}
+		if (isDoctor.value) {
+			return [
+				{ label: 'نام بیمار', value: 'username' },
+				{ label: 'شماره تلفن بیمار', value: 'phoneNumber' },
+				{ label: 'تاریخ نوبت', value: 'date' },
+				{ label: 'ساعت', value: 'hour' },
+				{ label: 'وضعیت', value: 'is_active' },
+			];
+		}
+	});
 
 	const removeReservation = async () => {
 		isModalOpen.value = false;
 		try {
-			deleteReservation(reservationId.value);
-
-			createTransaction('BANK', currentUser.value?.id, 300_000, 'REFUND');
+			const response = await deleteReservation(reservationId.value);
+			if (isPatient.value) {
+				createTransaction(
+					'BANK',
+					currentUser.value?.id,
+					response.data?.price,
+					'REFUND',
+				);
+			}
+			if (isDoctor.value) {
+				createTransaction(
+					'BANK',
+					userId.value,
+					response.data?.price,
+					'REFUND',
+				);
+			}
 
 			reservations.value = reservations.value.filter(
-				(item) => item.id !== reservationId.value,
+				(item) => item?.id !== reservationId.value,
 			);
 		} catch (error) {
 			if (
@@ -66,11 +99,22 @@
 
 		reservationId.value = row?.id;
 
-		modalText.value = `
-	     آیا از لغو نوبت دکتر ${row?.doctor_name}
-	     در تاریخ ${toPersianDigits(row?.date)}
-	     ساعت  ${toPersianDigits(row?.hour)}
-	     اطمینان دارید؟`;
+		if (isPatient.value) {
+			modalText.value = `
+				 آیا از لغو نوبت دکتر ${row?.doctor_name}
+				 در تاریخ ${toPersianDigits(row?.date)}
+				 ساعت  ${toPersianDigits(row?.hour)}
+				 اطمینان دارید؟`;
+		}
+		if (isDoctor.value) {
+			// userId.value = 5;
+
+			modalText.value = `
+				 آیا از لغو نوبت بیمار ${row?.username}
+				 در تاریخ ${toPersianDigits(row?.date)}
+				 ساعت  ${toPersianDigits(row?.hour)}
+				 اطمینان دارید؟`;
+		}
 		isModalOpen.value = true;
 	};
 
@@ -89,8 +133,11 @@
 
 			return {
 				id: reservation?.id,
-				// doctor_name: reservation?.doctor_name,
-				doctor_name: reservation?.username,
+				...(isPatient.value && { doctor_name: reservation?.doctor_name }),
+				...(isDoctor.value && { username: reservation?.patient_username }),
+				...(isDoctor.value && {
+					phoneNumber: reservation?.patient_phonenumber,
+				}),
 				is_active: reservation?.is_past ? 'منقضی' : 'فعال',
 				date: persianDate,
 				hour: `${hour} تا ${hour + 1}`,
